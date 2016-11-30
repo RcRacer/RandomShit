df6c3a28f7839c22413d282fa160c3ca66e429de
Ben james <everythingisasome@gmail.com>
2016-11-30 17:42:23 -0500

    Scheduled the method divreWithJoysticks

--- src/Commands/Drive.cpp
+++ src/Commands/Drive.cpp
@@ -30,7 +30,7 @@ void Drive::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void Drive::Execute() {
-
+	Robot::driveBase->driveWithJoysticks();
 }
 
 // Make this return true when this Command no longer needs to run execute()
